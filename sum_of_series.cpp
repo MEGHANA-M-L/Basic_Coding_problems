c8f4c45f0b56e054a6e0ce4c502c939c295824ec
MEGHANA-M-L <97900180+MEGHANA-M-L@users.noreply.github.com>
2024-05-09 20:26:49 +0530

    sum_of_series.cpp

--- sum_of_series.cpp
+++ sum_of_series.cpp
@@ -1,21 +1,24 @@
+//Write a program to find the sum of the given series 1+2+3+ . . . . . .(N terms) 
+/*Complete the function seriesSum() which takes single integer n, as input parameters and returns an integer denoting the answer. The result size may be large, so return a container that accommodates a 64-bit integer. You don't need to print the answer or take inputs.
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 109
+  */
 //{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
-
-
 // } Driver Code Ends
-
 class Solution {
   public:
     long long seriesSum(int n) {
         // code here
         long long l=n;
         return (l*(l+1))/2;
-        
     }
 };
-
-
 //{ Driver Code Starts.
 
 int main(){
